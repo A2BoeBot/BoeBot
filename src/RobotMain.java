9fb2c3e26845fbdff63bebc1ed8f3d872196db6e
Bownuz <fedde.snelder@gmail.com>
2023-11-16 14:10:39 +0100

    java versie aangepast

--- src/RobotMain.java
+++ src/RobotMain.java
@@ -4,14 +4,19 @@ import TI.PinMode;
 public class RobotMain {
 
     public static void main(String[] args) {
+        Motors moters = new Motors(new Servo(12), new Servo(13), 0,0);
+        moters.setSnelheid(200,50);
 
-        boolean state = true;
-        BoeBot.setMode(0, PinMode.Output);
+        moters.stop();
 
-        while (true) {
-            state = !state;
-            BoeBot.digitalWrite(0, state);
-            BoeBot.wait(250);
-        }
+//        moters.setSnelheid(0,50);
+//        boolean state = true;
+//        BoeBot.setMode(0, PinMode.Output);
+//
+//        while (true) {
+//            state = !state;
+//            BoeBot.digitalWrite(0, state);
+//            BoeBot.wait(250);
+//        }
     }
 }
