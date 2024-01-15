d5dfbb29bc0b60d0f52f27673fbda812937bd376
Bownuz <fedde.snelder@gmail.com>
2024-01-15 13:47:31 +0100

    "Afstandsbediening werkt nu (is nog niet aan de rest van de code gekoppeld)"

--- src/robot/Afstandsbediening.java
+++ src/robot/Afstandsbediening.java
@@ -3,29 +3,33 @@ package robot;
 import TI.BoeBot;
 import TI.PinMode;
 
+import java.util.Arrays;
+
 public class Afstandsbediening implements Updatable {
-    public static final int KNOP_1 = 0b10000000;
-    public static final int KNOP_2 = 0b10000001;
-    public static final int KNOP_3 = 0b10000010;
-    public static final int KNOP_4 = 0b10000011;
-    public static final int KNOP_5 = 0b10000100;
-    public static final int KNOP_6 = 0b10000101;
-    public static final int KNOP_7 = 0b10000110;
-    public static final int KNOP_8 = 0b10000111;
-    public static final int KNOP_9 = 0b10001000;
-    public static final int KNOP_0 = 0b10001001;
-    public static final int KNOP_UIT = 0b10010101;
-    public static final int KNOP_CH_BOVEN = 0b10010000;
-    public static final int KNOP_CH_ONDER = 0b10010001;
-    public static final int KNOP_VOL_LINKS = 0b10010011;
-    public static final int KNOP_VOL_RECHTS = 0b10010010;
+    public static final int KNOP_1 = 0;
+    public static final int KNOP_2 = 0b1;
+    public static final int KNOP_3 = 0b10;
+    public static final int KNOP_4 = 0b11;
+    public static final int KNOP_5 = 0b100;
+    public static final int KNOP_6 = 0b101;
+    public static final int KNOP_7 = 0b110;
+    public static final int KNOP_8 = 0b111;
+    public static final int KNOP_9 = 0b1000;
+    public static final int KNOP_0 = 0b1001;
+    public static final int KNOP_UIT = 0b10101;
+    public static final int KNOP_CH_BOVEN = 0b10000;
+    public static final int KNOP_CH_ONDER = 0b10001;
+    public static final int KNOP_VOL_LINKS = 0b10011;
+    public static final int KNOP_VOL_RECHTS = 0b10010;
     private int pulseLen;
     private int pin;
     private int lijstVanBitjes[] = new int[12];
+    private AfstandsbedieningCallback callback;
 
-    public Afstandsbediening(int pin) {
+    public Afstandsbediening(int pin, AfstandsbedieningCallback callback) {
         this.pin = pin;
         BoeBot.setMode(pin, PinMode.Input);
+        this.callback = callback;
     }
 
 
@@ -35,6 +39,7 @@ public class Afstandsbediening implements Updatable {
             for (int i = 0; i < 12; i++) {
                 lijstVanBitjes[i] = BoeBot.pulseIn(this.pin, false, 20000);
             }
+            System.out.println(Arrays.toString(lijstVanBitjes));
             int code = 0;
             for (int i = 0; i < 12; i++) {
                 if (lijstVanBitjes[i] > 1000) {
@@ -43,58 +48,53 @@ public class Afstandsbediening implements Updatable {
             }
             int buttoncode = code & 0b1111111;
             int devicecode = (code >> 7) & 0b11111;
-            System.out.println(code);
-            System.out.println(Integer.toBinaryString(code));
-            switch (code) {
-                case KNOP_1:
-                    System.out.println("Knop 1");
-                    break;
+            switch (buttoncode) {
+                case KNOP_1: this.callback.knop_1_Ingedrukt(); break;
                 case KNOP_2:
-                    System.out.println("Knop 2");
+                    this.callback.knop_2_Ingedrukt();
                     break;
                 case KNOP_3:
-                    System.out.println("Knop 3");
+                    this.callback.knop_3_Ingedrukt();
                     break;
                 case KNOP_4:
-                    System.out.println("Knop 4");
+                    this.callback.knop_4_Ingedrukt();
                     break;
                 case KNOP_5:
-                    System.out.println("Knop 5");
+                    this.callback.knop_5_Ingedrukt();
                     break;
                 case KNOP_6:
-                    System.out.println("Knop 6");
+                    this.callback.knop_6_Ingedrukt();
                     break;
                 case KNOP_7:
-                    System.out.println("Knop 7");
+                    this.callback.knop_7_Ingedrukt();
                     break;
                 case KNOP_8:
-                    System.out.println("Knop 8");
+                    this.callback.knop_8_Ingedrukt();
                     break;
                 case KNOP_9:
-                    System.out.println("Knop 9");
+                    this.callback.knop_9_Ingedrukt();
                     break;
                 case KNOP_0:
-                    System.out.println("Knop 0");
+                    this.callback.knop_0_Ingedrukt();
                     break;
                 case KNOP_UIT:
-                    System.out.println("Knop uit");     //Noodstop
+                    this.callback.knop_Uit_Ingedrukt();
                     break;
                 case KNOP_CH_BOVEN:
-                    System.out.println("Knop CH+");
+                    this.callback.knop_Ch_BovenIngedrukt();
                     break;
                 case KNOP_CH_ONDER:
-                    System.out.println("Knop CH-");
+                    this.callback.knop_Ch_OnderIngedrukt();
                     break;
                 case KNOP_VOL_LINKS:
-                    System.out.println("Knop VOL-");
+                    this.callback.knop_Vol_Links_Ingedrukt();
                     break;
                 case KNOP_VOL_RECHTS:
-                    System.out.println("Knop VOL+");
+                    this.callback.knop_Vol_Rechts_Ingedrukt();
                     break;
                 default:
                     break;
             }
         }
-        BoeBot.wait(10);
     }
 }
